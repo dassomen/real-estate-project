620d7640a498d14164290ae03258bda450223336
dassomen <somend265@gmail.com>
2024-04-25 02:49:38 +0530

    25/04/2024 Hero Section Complete 1st commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 export default {
   content: ["./*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontsize:{
+        sm:"0.75rem",
+        base:"0.875rem",
+        md:"1rem",
+        ms:"1.125rem",
+        mt:"1.25rem",
+        mu:"1.375rem",
+        mv:"1.5rem",
+        mx:"1.625rem",
+        mw:"1.75rem",
+        my:"2.25rem",
+      }
+    },
   },
   plugins: [],
 }
